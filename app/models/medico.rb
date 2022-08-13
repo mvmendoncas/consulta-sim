029bf90392585dfaad774bc2117f49c043d2eb7e
mvmendoncas <mvmendonca.s3301@gmail.com>
2022-08-13 17:34:11 -0300

    Validações do médico

--- app/models/medico.rb
+++ app/models/medico.rb
@@ -1,3 +1,9 @@
 class Medico < ApplicationRecord
   has_many :consultums, :dependent => :destroy
+  validates :email, presence: true, length: {minimum: 11}, uniqueness: true
+  validates :cpf, presence: true, numericality: { only_integer: true }, uniqueness: true, length: {is: 11}, exclusion: { in: %w(!@#$%¨&*()_+=[{]};:?/'")}
+  validates :primeiro_nome, presence: true, format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" },  length: { minimum: 2 }
+  validates :ultimo_nome, presence: true, format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" },  length: { minimum: 2 }
+  validates :numero_do_crm, presence: true, numericality: { only_integer: true }, uniqueness: true, length: {is: 6}, exclusion: { in: %w(!@#$%¨&*()_+=[{]};:?/'")}
+  validates :especialidade, presence: true, format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }, exclusion: { in: %w(!@#$%¨&*()_+=[{]};:?/'")},  length: { minimum: 2 }
 end
