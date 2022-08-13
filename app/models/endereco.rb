215ba9043ba60b5580a205fecc0312eec9c20917
mvmendoncas <mvmendonca.s3301@gmail.com>
2022-08-13 17:24:35 -0300

    Validações do paciente

--- app/models/endereco.rb
+++ app/models/endereco.rb
@@ -1,3 +1,8 @@
 class Endereco < ApplicationRecord
   belongs_to :paciente
+  validates :cep, presence: true, length: {is: 8}, numericality: { only_integer: true }
+  validates :cidade,  presence: true, format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }, exclusion: { in: %w(!@#$%¨&*()_+=[{]};:?/'")}
+  validates :bairro, presence: true, format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }, exclusion: { in: %w(!@#$%¨&*()_+=[{]};:?/'")}
+  validates :logradouro, presence: true, format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }, exclusion: { in: %w(!@#$%¨&*()_+=[{]};:?/'")}
+  validates :complemento, presence: true, format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }, exclusion: { in: %w(!@#$%¨&*()_+=[{]};:?/'")}
 end
--- app/models/paciente.rb
+++ app/models/paciente.rb
@@ -1,4 +1,10 @@
 class Paciente < ApplicationRecord
   has_one :endereco
   has_many :consultums, :dependent => :destroy
+
+  validates :primeiro_nome, presence: true, format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" },  length: { minimum: 2 }
+  validates :ultimo_nome, presence: true, format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" },  length: { minimum: 2 }
+  validates :cpf, presence: true, numericality: { only_integer: true }, uniqueness: true, length: {is: 11}
+  validates :email, presence: true, length: {minimum: 11}, uniqueness: true
+  validates :data_de_nascimento, presence: true
 end
