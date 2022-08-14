8ade3796227158174ba847630c5250f2f61b5dc8
mvmendoncas <mvmendonca.s3301@gmail.com>
2022-08-13 22:02:32 -0300

    Correção das validações do paciente

--- app/models/paciente.rb
+++ app/models/paciente.rb
@@ -2,9 +2,9 @@ class Paciente < ApplicationRecord
   has_one :endereco
   has_many :consultums, :dependent => :destroy
 
-  validates :primeiro_nome, presence: true, format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" },  length: { minimum: 2 }
-  validates :ultimo_nome, presence: true, format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" },  length: { minimum: 2 }
-  validates :cpf, presence: true, numericality: { only_integer: true }, uniqueness: true, length: {is: 11}
-  validates :email, presence: true, length: {minimum: 11}, uniqueness: true
-  validates :data_de_nascimento, presence: true
+  validates :primeiro_nome, presence: true, format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }, length: { minimum: 2 }
+  validates :ultimo_nome, presence: true, format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }, length: { minimum: 2 }
+  validates :cpf, presence: true, numericality: { only_integer: true }, uniqueness: true, length: { is: 11 }, exclusion: { in: %w(!@#$%¨&*()_+=[{]};:?/'") }
+  validates :email, presence: true, length: { minimum: 11 }, uniqueness: true
+  validates :data_de_nascimento, presence: true, presence: { message: "Data de nascimento é obrigatória!" }, validates_comparison_of: { less_than: DateTime.now, message: "Data de nascimento inválida" }
 end
