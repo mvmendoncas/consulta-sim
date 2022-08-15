6ef01d9c5dc6528c40099a0aa69c16a9ec9acdab
mvmendoncas <mvmendonca.s3301@gmail.com>
2022-08-15 11:52:43 -0300

    Retirando como comentário das validações

--- app/models/endereco.rb
+++ app/models/endereco.rb
@@ -1,9 +1,9 @@
 class Endereco < ApplicationRecord
   has_one :paciente
 
-  #validates :cep, presence: true, length: {is: 8}, numericality: { only_integer: true }
-  #validates :cidade,  presence: true, format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }, exclusion: { in: %w(!@#$%¨&*()_+=[{]};:?/'")},  length: { minimum: 2 }
-  #validates :bairro, presence: true, format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }, exclusion: { in: %w(!@#$%¨&*()_+=[{]};:?/'")},  length: { minimum: 2 }
-  # validates :logradouro, presence: true, format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }, exclusion: { in: %w(!@#$%¨&*()_+=[{]};:?/'")},  length: { minimum: 2 }
-  # validates :complemento, presence: true, format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }, exclusion: { in: %w(!@#$%¨&*()_+=[{]};:?/'")},  length: { minimum: 2 }
+  validates :cep, presence: true, length: {is: 8}, numericality: { only_integer: true }
+  validates :cidade,  presence: true, format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }, exclusion: { in: %w(!@#$%¨&*()_+=[{]};:?/'")},  length: { minimum: 2 }
+  validates :bairro, presence: true, format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }, exclusion: { in: %w(!@#$%¨&*()_+=[{]};:?/'")},  length: { minimum: 2 }
+  validates :logradouro, presence: true, format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }, exclusion: { in: %w(!@#$%¨&*()_+=[{]};:?/'")},  length: { minimum: 2 }
+  validates :complemento, presence: true, format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }, exclusion: { in: %w(!@#$%¨&*()_+=[{]};:?/'")},  length: { minimum: 2 }
 end
--- app/models/paciente.rb
+++ app/models/paciente.rb
@@ -4,9 +4,9 @@ class Paciente < ApplicationRecord
   has_many :consultums,  dependent: :destroy
   accepts_nested_attributes_for :consultums
 
-  #validates :primeiro_nome, presence: true, format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }, length: { minimum: 2 }
-  # validates :ultimo_nome, presence: true, format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }, length: { minimum: 2 }
-  # validates :cpf, presence: true, numericality: { only_integer: true }, uniqueness: true, length: { is: 11 }, exclusion: { in: %w(!@#$%¨&*()_+=[{]};:?/'") }
-  #validates :email, presence: true, length: { minimum: 11 }, uniqueness: true
-  # validates :data_de_nascimento, presence: true, presence: { message: "Data de nascimento é obrigatória!" }, comparison: { less_than: DateTime.now, message: "Data de nascimento inválida" }
+  validates :primeiro_nome, presence: true, format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }, length: { minimum: 2 }
+  validates :ultimo_nome, presence: true, format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }, length: { minimum: 2 }
+  validates :cpf, presence: true, numericality: { only_integer: true }, uniqueness: true, length: { is: 11 }, exclusion: { in: %w(!@#$%¨&*()_+=[{]};:?/'") }
+  validates :email, presence: true, length: { minimum: 11 }, uniqueness: true
+  validates :data_de_nascimento, presence: true, presence: { message: "Data de nascimento é obrigatória!" }, comparison: { less_than: DateTime.now, message: "Data de nascimento inválida" }
 end
