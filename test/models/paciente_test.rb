af414b4271f77adb93cca9f3ba156f1a937876e7
mvmendoncas <mvmendonca.s3301@gmail.com>
2022-08-28 18:21:45 -0300

    Corrigindo branchs

--- test/models/paciente_test.rb
+++ test/models/paciente_test.rb
@@ -1,14 +1,7 @@
 require "test_helper"
 
 class PacienteTest < ActiveSupport::TestCase
-  test 'salvar paciente sem cpf' do
-    paciente = Paciente.new
-    assert_not paciente.save
-  end
-  test 'salvar paciente com cpf' do
-    paciente = Paciente.new cpf: '12345678901'
-    assert paciente.save
-  end
+
   # test "the truth" do
   #   assert true
   # end
