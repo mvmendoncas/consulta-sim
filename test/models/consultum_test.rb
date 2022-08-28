f8334c4567b94f0ba43310dfeffc769fa9801d86
mvmendoncas <mvmendonca.s3301@gmail.com>
2022-08-28 18:35:33 -0300

    Testes no model de consulta

--- test/models/consultum_test.rb
+++ test/models/consultum_test.rb
@@ -1,6 +1,18 @@
 require "test_helper"
 
+
 class ConsultumTest < ActiveSupport::TestCase
+
+  test 'salvar consulta sem data' do
+    consulta = Consultum.new
+    assert_not consulta.save
+  end
+
+  test 'salvar consutla com data' do
+    consulta = Consultum.new '01/09/2022'
+    assert consulta.save
+  end
+
   # test "the truth" do
   #   assert true
   # end
