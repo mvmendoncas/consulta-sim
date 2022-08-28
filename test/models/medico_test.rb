9f7b11556b72a6f42925add77f9206874c76fb11
mvmendoncas <mvmendonca.s3301@gmail.com>
2022-08-28 18:24:13 -0300

    Testes no model do médico

--- test/models/medico_test.rb
+++ test/models/medico_test.rb
@@ -1,7 +1,14 @@
 require "test_helper"
 
 class MedicoTest < ActiveSupport::TestCase
- 
+  test 'salvar médico sem crm' do
+    medico = Medico.new
+    assert_not medico.save
+  end
+  test 'salvar médico com crm' do
+    medico = Medico.new crm: '123456'
+    assert medico.save
+  end
   # test "the truth" do
   #   assert true
   # end
