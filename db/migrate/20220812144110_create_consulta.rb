585baef4a7d87c0ccea46c24b1e4091438f3719f
mvmendoncas <mvmendonca.s3301@gmail.com>
2022-08-29 20:08:25 -0300

    conserto no banco de dados na tabela consulta

--- db/migrate/20220812144110_create_consulta.rb
+++ db/migrate/20220812144110_create_consulta.rb
@@ -1,6 +1,6 @@
 class CreateConsulta < ActiveRecord::Migration[7.0]
   def change
-    create_table :consultum do |t|
+    create_table :consulta do |t|
       t.date :data
       t.time :horario
 
--- db/migrate/20220812192231_add_medico_to_consulta.rb
+++ db/migrate/20220812192231_add_medico_to_consulta.rb
@@ -1,5 +1,5 @@
 class AddMedicoToConsulta < ActiveRecord::Migration[7.0]
   def change
-    add_reference :consultum, :medico, null: false, foreign_key: true
+    add_reference :consulta, :medico, null: false, foreign_key: true
   end
 end
