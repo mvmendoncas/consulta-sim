21a97a575b6be36bf85e6b5b4033c5ff5f558e5f
mvmendoncas <mvmendonca.s3301@gmail.com>
2022-08-30 10:27:05 -0300

    Teste no model de paciente

--- test/models/paciente_test.rb
+++ test/models/paciente_test.rb
@@ -1,7 +1,14 @@
 require "test_helper"
 
 class PacienteTest < ActiveSupport::TestCase
-
+  test'criando paciente sem o primeiro nome' do
+    paciente = Paciente.new
+    assert_not paciente.save
+  end
+  test 'Criando paciente com o primeiro nome' do
+    paciente = Paciente.new primeiro_nome: 'maria'
+    assert paciente.save
+  end
   # test "the truth" do
   #   assert true
   # end
