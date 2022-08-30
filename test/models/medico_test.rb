69e646824bb66d19b58033c68e5aa9f097cacc4f
mvmendoncas <mvmendonca.s3301@gmail.com>
2022-08-30 10:26:53 -0300

    Teste no model de medico

--- test/models/medico_test.rb
+++ test/models/medico_test.rb
@@ -1,7 +1,14 @@
 require "test_helper"
 
 class MedicoTest < ActiveSupport::TestCase
- 
+  test 'Criando médico com crm menor que 6 digitos' do
+    medico = Medico.new crm: '12345'
+    assert medico.saved_change_to_numero_do_crm
+  end
+  test 'Criando médico com crm de 6 digitos' do
+    medico = Medico.new crm: '123456'
+    assert medico.saved_change_to_numero_do_crm
+  end
   # test "the truth" do
   #   assert true
   # end
