96434b606d8be0d35a0c2b107f6b1a63914809b0
mvmendoncas <mvmendonca.s3301@gmail.com>
2022-08-30 10:26:38 -0300

    Teste no model de consulta

--- test/models/consultum_test.rb
+++ test/models/consultum_test.rb
@@ -1,6 +1,16 @@
 require "test_helper"
 
 class ConsultumTest < ActiveSupport::TestCase
+  test 'Criando uma consulta'  do
+    get '/consulta/new'
+    assert_response :success
+    post '/consulta', params: {consulta: {medico: 'roberto', paciente: 'maria', data: '2022-08-12', horario: '2022-08-12 11:41:10'}}
+    assert_select 'Medico:\n roberto'
+  end
+  test 'Criando uma consulta sem informa data' do
+    consulta = Consulta.new
+    assert_not consulta.save
+  end
   # test "the truth" do
   #   assert true
   # end
